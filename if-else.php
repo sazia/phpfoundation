b11734d5b6bc31e66bef57a107e26cc45aee4077
sazia <sazia_11yahoo.com>
2014-07-09 14:42:23 +0600

    crade check

---
+++ if-else.php
@@ -0,0 +1,28 @@
+ <?php
+ 
+ $marks=43;
+ 
+ if($marks>=80 && $marks<=100)
+ {
+   echo "grade A+";
+ }
+ else
+ if($marks>=70 && $marks<=79)
+ {
+   echo "grade A";
+ }
+ else
+ if($marks>=60 && $marks<=69)
+ {
+   echo "grade A-";
+ }
+ else
+ if($marks<=50 && $marks<=59)
+ {
+   echo "grade B";
+ } 
+ else
+ if($marks<=40 && $marks<=49)
+ {
+   echo"grade c";
+ } 
